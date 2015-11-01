aff92edc9861093660e99d6edb3a51fa1fbe062b
Shinichi Kogai <kogai0121@gmail.com>
2015-11-01 15:45:38 +0900

    挿入ソート

--- insert.go
+++ insert.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	// "fmt"
 	"github.com/kogai/algorithms/helper"
 )
 
 func main() {
-	helper.Benchmark(sort, 10000) // 平均値: 48.565721ms
+	helper.Benchmark(sort, 10000) // 平均値: 165.659119ms
 }
 
-func sort(a []int) []int {
-	count := len(a)
-	for index := 1; index < count; index++ {
-		insert(a, index, a[index])
-	}
-	return a
-}
+func sort(arr []int) []int {
+	max := len(arr)
 
-func insert(arr []int, pos int, val int) {
-	i := pos - 1
-	for i >= 0 && helper.Compare(arr[i], val) > 0 {
-		arr[i+1] = arr[i]
-		i--
+	for i := 1; i < max; i++ {
+		for index := 0; index < i; index++ {
+			if helper.Compare(arr[index], arr[i]) > 0 {
+				tmp := arr[index]
+				arr[index] = arr[i]
+				arr[i] = tmp
+			}
+		}
 	}
-	arr[i+1] = val
+
+	return arr
 }
